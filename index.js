13ded12bb824a66246b69a5b577fe3ad54caa533
Ikram Chouichi <ikram@netronian.com>
2023-12-18 21:23:49 -0500

    finish-lab

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = function() {
+    return 2000 / 100;
+  }
+  const square = (x) => {
+    return x * x;
+  }
+
+const add = (parameter1, parameter2) => 
+{ return parameter1 + parameter2;}
+  
